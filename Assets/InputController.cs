443306d5916899cfbee10c1478f5f5e68fc32180
SolidHinken <a.arutyunyan@gamedelta.io>
2020-06-07 17:22:26 +0300

    07.06.20
    Added karting minigame assets
    Input Controller
    Camera

---
+++ Assets/InputController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+
+    public float inputRate = 0.5f;
+    public float playerInput;
+    public bool CorrectInput = false;
+
+    [Tooltip("Percentage of the screen from bottom")]
+    public float startPosTreshold = 1f;
+    void Update()
+    {
+        if (true)
+        {
+            
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                Vector3 startPos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+                //Debug.Log(startPos);
+                if (Mathf.Abs(startPos.x) <= startPosTreshold)
+                {
+                    CorrectInput = true;
+                }
+                else
+                {
+                    CorrectInput = false;
+                }
+            }
+            else if (Input.GetMouseButton(0) && CorrectInput)
+            {
+                    playerInput = Mathf.Clamp(Input.GetAxis("Mouse X"), -10f, 10f)*inputRate;
+            }
+            else if (Input.GetMouseButtonUp(0))
+            {
+                playerInput = 0f;
+            }
+        }
+
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using PathCreation.Examples;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : Singleton<PlayerController>
+{
+
+    public Transform playerHolder;
+
+    public PathFollower playerFollower;
+    public PathFollower cameraFollower;
+
+    [SerializeField] private InputController _inputController;
+    [SerializeField] private float playerSpeed = 5f;
+
+
+
+    public float PlayerSpeed
+    {
+        get
+        {
+            return playerSpeed;
+        }
+
+        set
+        {
+            if(playerSpeed != value)
+            {
+            }
+          
+                playerSpeed = value;
+                InitializePathFollower();
+        }
+    }
+
+
+    private void Start()
+    {
+        InitializePathFollower();
+
+    }
+
+
+    public void InitializePathFollower()
+    {
+        playerFollower.speed = playerSpeed;
+ 
+        cameraFollower.distanceTravelled = playerFollower.distanceTravelled + 5f;
+        cameraFollower.speed = playerSpeed;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerHolder.localPosition += Vector3.up* _inputController.playerInput;
+    }
+}
