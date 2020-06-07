ccba159571a781a2babeee40ed6d445380ff15a3
SolidHinken <a.arutyunyan@gamedelta.io>
2020-06-07 17:37:12 +0300

    07.06.20
    Cuberotator

---
+++ Assets/SimpleRotator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleRotator : MonoBehaviour
+{
+    [SerializeField] private float speedX = 0f;
+    [SerializeField] private float speedY = 0f;
+    [SerializeField] private float speedZ = 0f;
+    [SerializeField] private bool IsWorldSpace = false;
+
+    void Update()
+    {
+        transform.Rotate(speedX * Time.deltaTime, speedY * Time.deltaTime, speedZ * Time.deltaTime, IsWorldSpace?Space.World:Space.Self);
+
+    }
+
+}
